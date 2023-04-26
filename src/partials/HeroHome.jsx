0eb0228c6ac3d6752c53ae469fd88e823ae21a7e
ayubero <32571894+ayubero@users.noreply.github.com>
2023-04-26 22:35:26 +0100

    Update HeroHome.jsx

--- src/partials/HeroHome.jsx
+++ src/partials/HeroHome.jsx
@@ -36,13 +36,13 @@ function HeroHome() {
           {/* Section header */}
           <div className="text-center pb-12 md:pb-16">
             <h1 className="text-5xl md:text-6xl font-extrabold leading-tighter tracking-tighter mb-4" data-aos="zoom-y-out">
-              We make <span className="bg-clip-text text-transparent bg-gradient-to-r from-blue-500 to-teal-400">rockets</span>
+              Ad <span className="bg-clip-text text-transparent bg-gradient-to-r from-blue-500 to-teal-400">lunam</span>!
             </h1>
             <div className="max-w-3xl mx-auto">
               <p className="text-xl text-gray-600 mb-8" data-aos="zoom-y-out" data-aos-delay="150">
                 Official website of the rocketry team of the University of Zaragoza.
               </p>
-              <div className="max-w-xs mx-auto sm:max-w-none sm:flex sm:justify-center" data-aos="zoom-y-out" data-aos-delay="300">
+{/*<div className="max-w-xs mx-auto sm:max-w-none sm:flex sm:justify-center" data-aos="zoom-y-out" data-aos-delay="300">
                 <div>
                   <a className="btn text-white bg-blue-600 hover:bg-blue-700 w-full mb-4 sm:w-auto sm:mb-0" href="#0">
                     Conócenos
@@ -78,7 +78,7 @@ function HeroHome() {
                     Contacto
                   </a>
                 </div>
-              </div>
+              </div>*/}
             </div>
           </div>
 
