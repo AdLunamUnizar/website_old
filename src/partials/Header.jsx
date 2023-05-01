77f555efffe4ab90bd6610e61c5971133b8dd7cf
4n-dr3s <114180227+4n-dr3s@users.noreply.github.com>
2023-05-01 15:17:02 +0200

    Update Header.jsx

--- src/partials/Header.jsx
+++ src/partials/Header.jsx
@@ -43,10 +43,10 @@ function Header() {
           <nav className="flex flex-grow">
             <ul className="flex flex-grow justify-end flex-wrap items-center">
               <li>
-                <Link to="mailto:alunizar@unizar.es" className="font-medium text-gray-600 hover:text-gray-900 px-5 py-3 flex items-center transition duration-150 ease-in-out">Contact</Link>
+                <Link to="mailto:alunizar@unizar.es" target="_blank" className="font-medium text-gray-600 hover:text-gray-900 px-5 py-3 flex items-center transition duration-150 ease-in-out">Contact</Link>
               </li>
               <li>
-                <Link to="https://docs.google.com/forms/d/1me2YhfKRXGim-WILX1wPTUj3TOheu39S1PSdAOTeuAo/edit?usp=drivesdk" className="btn-sm text-gray-200 bg-gray-900 hover:bg-gray-800 ml-3">
+                <Link to="https://docs.google.com/forms/d/1me2YhfKRXGim-WILX1wPTUj3TOheu39S1PSdAOTeuAo/edit?usp=drivesdk" target="_blank" className="btn-sm text-gray-200 bg-gray-900 hover:bg-gray-800 ml-3">
                   <span>Join</span>
                   <svg className="w-3 h-3 fill-current text-gray-400 flex-shrink-0 ml-2 -mr-1" viewBox="0 0 12 12" xmlns="http://www.w3.org/2000/svg">
                     <path d="M11.707 5.293L7 .586 5.586 2l3 3H0v2h8.586l-3 3L7 11.414l4.707-4.707a1 1 0 000-1.414z" fillRule="nonzero" />
